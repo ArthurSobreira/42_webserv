767b5d5565f8c4d18d12000c9b0b976411e73016
phenriq2 <phenriq2@student.42sp.org.br>
2024-08-26 17:51:01 -0300

    testing with an ip passed by line of code

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,27 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 18:29:45 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/08/26 17:13:05 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/08/26 17:40:44 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/includes.hpp"
 
-void ft_error(const char *message, const char *error_detail, const char *function, const char *file, int line, int error_code)
+void responseCreate(int client_socket, std::string content)
 {
-	std::cerr << "Erro: " << message << " (" << error_detail << ")"
-			  << " in function " << function << " at " << file << ":" << line
-			  << " Error code: " << error_code << std::endl;
+	std::ostringstream response;
+	response << "HTTP/1.1 200 OK\r\n";
+	response << "Content-Type: text/html\r\n";
+	response << "Content-Length: " << content.size() << "\r\n";
+	response << "\r\n";
+	response << content;
+	send(client_socket, response.str().c_str(), response.str().size(), 0);
 }
+
 
 int main(void)
 {
-	std::string ip = "127.0.0.1";
+	std::string ip = "10.11.9.4";
 	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_socket == -1)
 	{
@@ -29,12 +34,11 @@ int main(void)
 		return 1;
 	}
 	std::cout << "Socket criado com sucesso!" << std::endl;
-
 	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
-	server_address.sin_addr.s_addr = INADDR_ANY; 
+	inetPton(ip, server_address.sin_addr.s_addr);
+	std::cout << "IP: " << server_address.sin_addr.s_addr << std::endl;
 	server_address.sin_port = htons(13000);		 
-
 	if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
 	{
 		ft_error("Erro ao fazer bind", strerror(errno), __FUNCTION__, __FILE__, __LINE__, errno);
@@ -48,39 +52,25 @@ int main(void)
 		close(server_socket);
 		return 1;
 	}
-
 	std::cout << "Servidor escutando na porta 13000..." << std::endl;
 	std::ifstream file("../static/index.html");
 	std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-
 	while (true)
 	{
 		struct sockaddr_in client_address;
 		socklen_t client_len = sizeof(client_address);
 		int client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_len);
-
 		if (client_socket < 0)
 		{
 			perror("accept failed");
 			close(server_socket);
 			return 1;
 		}
-
 		std::cout << "Cliente conectado!" << std::endl;
-
-		// Construindo a resposta HTTP completa
-		std::ostringstream response;
-		response << "HTTP/1.1 200 OK\r\n";
-		response << "Content-Type: text/html\r\n";
-		response << "Content-Length: " << content.size() << "\r\n";
-		response << "\r\n";
-		response << content;
-		std::string response_str = response.str();
-		send(client_socket, response_str.c_str(), response_str.size(), 0);
+		responseCreate(client_socket, content);
 		close(client_socket);
 		std::cout << "Conexão com o cliente fechada." << std::endl;
 	}
-
 	close(server_socket);
 	return 0;
 }
