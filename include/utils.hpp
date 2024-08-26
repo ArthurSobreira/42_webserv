3411735c2adf66f63fb095471b74a4fb5d10320f
phenriq2 <phenriq2@student.42sp.org.br>
2024-08-26 17:54:29 -0300

    functions I sent to utils.hpp

--- include/utils.hpp
+++ include/utils.hpp
@@ -2,7 +2,6 @@
 #define UTILS_HPP
 
 bool inetPton(const std::string &ip_str, uint32_t &out_binary_ip);
-
-
+void ft_error(const char *message, const char *error_detail, const char *function, const char *file, int line, int error_code);
 
 #endif // UTILS_HPP
