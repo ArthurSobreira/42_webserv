2295227d409b17695f2e10287281dc1cef6777d3
phenriq2 <phenriq2@student.42sp.org.br>
2024-08-26 17:52:17 -0300

    relocation of the ft_error function and creation of a function to convert ip to uint32

--- src/utils.cpp
+++ src/utils.cpp
@@ -21,12 +21,15 @@ bool inetPton(const std::string &ip_str, uint32_t &out_binary_ip)
 			return false;
 		}
 	}
-
 	if (bytes.size() != 4)
-	{
-		std::cerr << "Endereço IP inválido." << std::endl;
 		return false;
-	}
-	out_binary_ip = (bytes[0] << 24) | (bytes[1] << 16) | (bytes[2] << 8) | bytes[3];
+	out_binary_ip = htonl((bytes[0] << 24) | (bytes[1] << 16) | (bytes[2] << 8) | bytes[3]);
 	return true;
+}
+
+void ft_error(const char *message, const char *error_detail, const char *function, const char *file, int line, int error_code)
+{
+	std::cerr << "Erro: " << message << " (" << error_detail << ")"
+			  << " in function " << function << " at " << file << ":" << line
+			  << " Error code: " << error_code << std::endl;
 }
