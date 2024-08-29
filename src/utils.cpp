67f98458dc9c4c12c8ef00770433bdf0fbf1901e
phenriq2 <phenriq2@student.42sp.org.br>
2024-08-29 20:04:27 -0300

    implementation of the function to revert uint32 to string

--- src/utils.cpp
+++ src/utils.cpp
@@ -22,6 +22,20 @@ bool inetPton(const std::string &ip_str, uint32_t &out_binary_ip)
 	return true;
 }
 
+
+std::string inetNtop(uint32_t binary_ip)
+{
+    uint32_t host_ip = ntohl(binary_ip);
+
+    int byte1 = (host_ip >> 24) & 0xFF;
+    int byte2 = (host_ip >> 16) & 0xFF;
+    int byte3 = (host_ip >> 8) & 0xFF;
+    int byte4 = host_ip & 0xFF;
+    std::ostringstream ip_stream;
+    ip_stream << byte1 << "." << byte2 << "." << byte3 << "." << byte4;
+    return ip_stream.str();
+}
+
 void ft_error(const char *message, const char *function, const char *file, int line, const std::exception& e)
 {
     std::ostringstream  oss;
