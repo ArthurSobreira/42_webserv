b3cef46240ddf6db5610f625b45193efa265306d
phenriq2 <phenriq2@student.42sp.org.br>
2024-09-05 19:33:07 -0300

    for now they are here

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,9 +1,14 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
-bool 		inetPton(const std::string &ip_str, uint32_t &out_binary_ip);
-void 		ft_error(const char *message, const char *function, const char *file, int line, const std::exception &e);
+class Logger;
+typedef struct sockaddr_in sockaddrIn;
+
+bool inetPton(const std::string &ip_str, uint32_t &out_binary_ip, Logger &logger);
+void ft_error(const char *message, const char *function, const char *file, int line, const std::exception &e);
 std::string inetNtop(uint32_t binary_ip);
 std::string readFile(const std::string &path);
+
+// ###################################//
 
 #endif // UTILS_HPP
