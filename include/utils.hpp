adfc6893cf2891c7e876fefd105a998523a4a1cf
phm-aguiar <phenriq2@student.42sp.org.br>
2024-09-12 13:11:50 -0300

    leave it there for now

--- include/utils.hpp
+++ include/utils.hpp
@@ -8,7 +8,9 @@ bool inetPton(const std::string &ip_str, uint32_t &out_binary_ip, Logger &logger
 void ft_error(const char *message, const char *function, const char *file, int line, const std::exception &e);
 std::string inetNtop(uint32_t binary_ip);
 std::string readFile(const std::string &path);
-std::string getTimestamp();
+std::string getContentType(const std::string &uri);
+std::string listDirectory(const std::string &dirPath);
+
 
 
 // ###################################//
