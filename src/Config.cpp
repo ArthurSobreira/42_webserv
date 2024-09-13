e6b3d2bb2a2019cbb7523491903f942029756158
Arthur Sobreira <tutusobreira@gmail.com>
2024-09-13 17:34:36 -0300

    Update Config.cpp

--- src/Config.cpp
+++ src/Config.cpp
@@ -1,16 +1,31 @@
+#include "Includes.hpp"
+#include "Defines.hpp"
 #include "Config.hpp"
 
-/* Constructor Methods */
-Config::Config( void ) { throw std::runtime_error("Error: File Not Passed"); };
+/* Constructor Method */
+Config::Config( const std::string &fileName, Logger &logger ) 
+	: _fileName(fileName), _logger(logger) {
+	std::ifstream configFile(fileName.c_str());
 
-Config::Config( const std::string &fileName ) : _fileName(fileName) {
-	std::cout << "Config file: " << _fileName << std::endl;
+	if (!configFile.is_open() || configFile.fail()) {
+		throw std::runtime_error(ERROR_OPEN_CONFIG_FILE);
+	}
+
+	_logger.logDebug("Config file: " + fileName, true);
+	if (configFile.is_open()) {
+		this->_parseConfigFile(configFile);
+		configFile.close();
+		return ;
+	}
 };
 
 /* Destructor Method */
 Config::~Config( void ) {};
 
 /* Public Methods */
-void Config::parseConfigFile( void ) {
-	return ;
+void Config::_parseConfigFile( std::ifstream &configFile ) {
+	std::string line;
+	while (std::getline(configFile, line)) {
+		_logger.logDebug(line);
+	}
 }
