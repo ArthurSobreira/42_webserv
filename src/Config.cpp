e9235932bd0bc2f7bd0d8794b0297895aacb5412
Arthur Sobreira <tutusobreira@gmail.com>
2024-09-13 11:28:09 -0300

    Update Config.cpp

--- src/Config.cpp
+++ src/Config.cpp
@@ -1,16 +1,16 @@
 #include "Config.hpp"
 
 /* Constructor Methods */
-Config::Config( void ) { throw std::runtime_error("Error: file not passed"); };
+Config::Config( void ) { throw std::runtime_error("Error: File Not Passed"); };
+
+Config::Config( const std::string &fileName ) : _fileName(fileName) {
+	std::cout << "Config file: " << _fileName << std::endl;
+};
 
 /* Destructor Method */
 Config::~Config( void ) {};
 
 /* Public Methods */
-// int Config::getAttribute( void ) const {
-// 	return (this->attribute);
-// }
-
-// void Config::setAttribute( int value ) {
-// 	this->attribute = value;
-// }
+void Config::parseConfigFile( void ) {
+	return ;
+}
