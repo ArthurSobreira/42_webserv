ded9eb6f1655adf2ed2efc9a4b21755679d8512d
Pedro Henrique Modesto De Aguiar <phenriq2@student.42sp.org.br>
2024-09-18 11:09:49 -0300

    I don't remember

--- src/Request.cpp
+++ src/Request.cpp
@@ -9,6 +9,22 @@ bool Request::parseRequest(const std::string &raw_request)
     {
         return false;
     }
+    std::string line;
+	std::getline(stream, line);
+    while (std::getline(stream, line))
+    {
+        size_t pos = line.find(": ");
+        if (pos != std::string::npos)
+        {
+            std::string key = line.substr(0, pos);
+            std::string value = line.substr(pos + 2);
+            headers[key] = value;
+			std::cout << "["<< key <<"]" << std::endl;
+			std::cout << "["<< value <<"]" << std::endl;
+        }
+    }
+    std::cout << "["<< headers["accept"] <<"]" << std::endl;
+
     return true;
 }
 Request::Request() : method(""), uri(""), http_version("") {}
