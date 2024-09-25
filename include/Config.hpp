f18635556a7295a3aff4e59db2606fbff5f4b286
Arthur Sobreira <tutusobreira@gmail.com>
2024-09-25 12:32:16 -0300

    Update Config.hpp

--- include/Config.hpp
+++ include/Config.hpp
@@ -80,7 +80,7 @@ namespace ConfigUtils {
 	std::string	trimServerBlock( const std::string &serverBlock );
 	std::string	shortToString( const short &value );
 	void	printServerStruct( const ServerConfigs &server );
-	bool	hostIsValid(std::vector<ServerConfigs> &servers);
+	bool	hostIsValid( ServerConfigs &server );
 }
 
 /* Server Extraction Functions */
