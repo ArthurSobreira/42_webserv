02ff609895b08634e4c1a42ac4c2559812fb8f84
Arthur Sobreira <tutusobreira@gmail.com>
2024-09-25 12:52:08 -0300

    Update ConfigUtils.cpp

--- src/ConfigUtils.cpp
+++ src/ConfigUtils.cpp
@@ -46,9 +46,7 @@ namespace ConfigUtils {
 			size_t last = line.find_last_not_of(' ');
 			line = line.substr(first, (last - first + 1));
 
-			if (line.empty()) {
-				continue;
-			}
+			if (line.empty()) { continue; }
 
 			if ((line.find("server") != std::string::npos &&
 				line.find("server_name") == std::string::npos) ||
@@ -72,6 +70,37 @@ namespace ConfigUtils {
 			timmedServerBlock << line << std::string("\n");
 		}
 		return (timmedServerBlock.str());
+	}
+
+	bool hostIsValid(ServerConfigs &server) {
+		std::istringstream stream(server.host);
+		std::vector<int> bytes;
+		std::string segment;
+		int fd = -1;
+		uint32_t ip;
+
+		if (!inetPton(server.host)) { 
+			return false; 
+		}
+		while (std::getline(stream, segment, '.')) {
+			int byte;
+			std::istringstream str(segment);
+			str >> byte;
+			bytes.push_back(byte);
+		}
+		ip = htonl((bytes[0] << 24) | (bytes[1] << 16) | (bytes[2] << 8) | bytes[3]);
+
+		createSocket(fd, AF_INET, SOCK_STREAM);
+		sockaddrIn serv_addr;
+		serv_addr.sin_family = AF_INET;
+		serv_addr.sin_addr.s_addr = ip;
+		serv_addr.sin_port = htons(19000);
+		if (bind(fd, (sockAddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+			close(fd);
+			return false;
+		}
+		close(fd);
+		return (true);
 	}
 
 	std::string	shortToString( const short &value ) {
@@ -119,39 +148,5 @@ namespace ConfigUtils {
 			std::cout << "\tCGI enabled: " << it->cgiConfig.cgiEnabled << std::endl;
 			std::cout << std::endl;
 		}
-	}
-
-	bool hostIsValid(std::vector<ServerConfigs> &servers) {
-		std::vector<int> bytes;
-		for (size_t i = 0; i < servers.size(); ++i)
-		{
-			if (!inetPton(servers[i].host))
-				return false;
-			std::istringstream stream(servers[i].host);
-			std::string segment;
-			int fd = -1;
-			uint32_t ip;
-			while (std::getline(stream, segment, '.'))
-			{
-				int byte;
-				std::istringstream str(segment);
-				str >> byte;
-				bytes.push_back(byte);
-			}
-			ip = (bytes[0] << 24) | (bytes[1] << 16) | (bytes[2] << 8) | bytes[3];
-
-			createSocket(fd, AF_INET, SOCK_STREAM);
-			sockaddrIn serv_addr;
-			serv_addr.sin_family = AF_INET;
-			serv_addr.sin_addr.s_addr = ip;
-			serv_addr.sin_port = htons(servers[i].port);
-			if (bind(fd, (sockAddr *)&serv_addr, sizeof(serv_addr)) < 0)
-			{
-				close(fd);
-				return false;
-			}
-			close(fd);
-		}
-		return true;
 	}
 }
