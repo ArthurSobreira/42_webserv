42783917931c871e5b442ad0b7763e1ec79cabd3
Pedro Henrique Modesto De Aguiar <phenriq2@student.42sp.org.br>
2024-09-28 13:50:03 -0300

    the amazing typedef logstream

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -12,6 +12,7 @@ typedef struct epoll_event epollEvent;
 typedef struct sockaddr sockAddr;
 typedef struct stat status;
 typedef struct dirent diretory;
+typedef std::ostringstream logStream;
 
 bool inetPton(const std::string &ip_str);
 void ft_error(const char *message, const char *function, 
@@ -20,10 +21,5 @@ std::string inetNtop(uint32_t binary_ip);
 std::string readFile(const std::string &path);
 std::string getContentType(const std::string &uri);
 std::string listDirectory(const std::string &dirPath);
-
-bool createSocket(int &sockfd, int domain, int protocol);
-bool bindSocket(int &sockfd, const int &port, const uint32_t &ip, sockaddrIn &serv_addr, Logger &logger);
-bool listenSocket(int &sockfd, int &backlog, Logger &logger);
-void createServer(int &sockfd, const int &port, const uint32_t &ip, int &backlog, Logger &logger);
 
 #endif
