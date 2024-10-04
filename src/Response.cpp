6bfda890885df641271ca44f103b177c766d246a
Pedro Henrique Modesto De Aguiar <phenriq2@student.42sp.org.br>
2024-10-04 10:31:35 -0300

    There is still a long way to go for this class

--- src/Response.cpp
+++ src/Response.cpp
@@ -1,104 +1,128 @@
-#include "Includes.hpp"
-#include "Request.hpp"
 #include "Response.hpp"
-#include "Logger.hpp"
 #include "Utils.hpp"
+#include <sys/stat.h>
+#include "Config.hpp"
 
+// Construtor padrão inicializando o status HTTP como 200 (OK)
 Response::Response() : status_code(200), reason_phrase("OK") {}
 
-std::string Response::generateResponse() const {
+// Gera a resposta HTTP completa
+std::string Response::generateResponse() const
+{
 	std::ostringstream response_stream;
 
 	response_stream << "HTTP/1.1 " << status_code << " " << reason_phrase << "\r\n";
-	for (std::map<std::string, std::string>::const_iterator it = headers.begin(); it != headers.end(); ++it) {
+	for (std::map<std::string, std::string>::const_iterator it = headers.begin(); it != headers.end(); ++it)
+	{
 		response_stream << it->first << ": " << it->second << "\r\n";
 	}
 	response_stream << "\r\n";
-	std::cout << response_stream.str() << std::endl;
 	response_stream << this->body;
 
 	return response_stream.str();
 }
 
-void Response::setStatus(int code, const std::string &reason) {
+// Define o status da resposta
+void Response::setStatus(int code, const std::string &reason)
+{
 	status_code = code;
 	reason_phrase = reason;
 }
 
-void Response::setHeader(const std::string &key, const std::string &value) {
+// Define um cabeçalho na resposta
+void Response::setHeader(const std::string &key, const std::string &value)
+{
 	headers[key] = value;
 }
 
-void Response::setBody(const std::string &bodyContent) {
+// Define o corpo da resposta e ajusta o Content-Length
+void Response::setBody(const std::string &bodyContent)
+{
 	body = bodyContent;
 	std::stringstream ss;
 	ss << body.size();
 	setHeader("Content-Length", ss.str());
 }
 
-int Response::getStatusCode() const {
+// Métodos de acesso
+int Response::getStatusCode() const
+{
 	return status_code;
 }
 
-std::string Response::getReasonPhrase() {
+std::string Response::getReasonPhrase() const
+{
 	return reason_phrase;
 }
 
-std::string Response::getHeader(const std::string &key) const {
+std::string Response::getHeader(const std::string &key) const
+{
 	std::map<std::string, std::string>::const_iterator it = headers.find(key);
-	if (it != headers.end()) {
+	if (it != headers.end())
+	{
 		return it->second;
 	}
 	return "";
 }
 
-std::string Response::getBody() const {
+std::string Response::getBody() const
+{
 	return body;
 }
 
-void Response::responseTratament(Request &request, Logger &logger){
-	std::ostringstream log;
-	if(!request.getIsRequestValid()){
-		logger.logError("ERROR", "Error parsing request");
-		std::string bodyr = readFile("static/405.html");
-		this->setStatus(405, "Method Not Allowed");
-		setHeader("Content-Type", getContentType("static/405.html"));
-		this->setBody(bodyr);
-		return ;
-	}
-	std::string path = "static" + request.getUri();
+// Define o corpo e o tipo de conteúdo (Content-Type)
+void Response::setBodyWithContentType(const std::string &bodyContent, const std::string &path)
+{
+	setBody(bodyContent);
+	setHeader("Content-Type", getContentType(path));
+}
+
+void Response::processRequest(Request &request, Logger &logger)
+{
+	std::string path;
+	if (request.getUri().find("static") != std::string::npos)
+		path = request.getUri();
+	else
+		path = "static" + request.getUri();
+	logger.logDebug(LOG_INFO, "'response' Request URI: " + path, true);
 	if (path[path.size() - 1] == '/')
 		path += "index.html";
-	std::string bodyr;
-	struct stat status;
-	if (stat(path.c_str(), &status) != 0)
+	status Status;
+	if (stat(path.c_str(), &Status) != 0)
 	{
-		logger.logError("ERROR", "File not found");
-		bodyr = readFile("static/404.html");
-		this->setStatus(404, "Not Found");
-		this->setBody(bodyr);
-		this->setHeader("Content-Type", getContentType("static/404.html"));
-		return ;
+		handleError(404, "static/404.html", "File not found", logger);
+		return;
 	}
-	if(S_ISDIR(status.st_mode) && request.getIsAllowDirectoryListing())
+	if (S_ISDIR(Status.st_mode) && request.getIsAllowDirectoryListing())
 	{
-		bodyr = listDirectory(path);
-		this->setStatus(200, "OK");
-		this->setBody(bodyr);
-		this->setHeader("Content-Type", getContentType("static/404.html"));
-		return ;
+		std::string directoryListing = listDirectory(path);
+
+		setBodyWithContentType(directoryListing, "directory.html");
+		setStatus(200, "OK");
+		return;
 	}
-	if(access(path.c_str(), R_OK) != 0)
+
+	if (access(path.c_str(), R_OK) != 0)
 	{
-		logger.logError("ERROR", "File not readable");
-		bodyr = readFile("static/403.html");
-		this->setStatus(403, "Forbidden");
-		this->setBody(bodyr);
-		this->setHeader("Content-Type", getContentType("static/403.html")); 
-		return ;
+		handleError(403, "static/403.html", "File not readable", logger);
+		return;
 	}
-	bodyr = readFile(path);
-	this->setStatus(200, "OK");
-	this->setHeader("Content-Type", getContentType(path));
-	setBody(bodyr);
+
+	handleFileResponse(path, logger);
+}
+
+void Response::handleError(int status_code, const std::string &error_page, const std::string &error_message, Logger &logger)
+{
+	logger.logError("ERROR", error_message);
+	std::string bodyContent = readFile(error_page);
+	setStatus(status_code, error_message);
+	setBodyWithContentType(bodyContent, error_page);
+}
+
+void Response::handleFileResponse(const std::string &path, Logger &logger)
+{
+	std::string bodyContent = readFile(path);
+	setStatus(200, "OK");
+	setBodyWithContentType(bodyContent, path);
+	logger.logDebug(LOG_INFO, "Response body: " + bodyContent);
 }
