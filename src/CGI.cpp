6225a0f2bd1d6c8019d539cebac62db237404474
Arthur Sobreira <tutusobreira@gmail.com>
2024-10-09 17:17:48 -0300

    Update CGI.cpp

--- src/CGI.cpp
+++ src/CGI.cpp
@@ -1,20 +1,34 @@
 
 #include "CGI.hpp"
 
-// Default constructor
-CGI::CGI() { return; }
+/* Constructor Method */
+CGI::CGI( const Request &request, const LocationConfigs &location ) 
+	: _request(request), _cgiConfig(location.cgiConfig) {
+	_returnCode = 0;
+	_returnbody = "";
 
-// Copy constructor
-CGI::CGI(const CGI &other) {
-  *this = other;
-  return;
+	Logger logger(LOG_FILE, LOG_ACCESS_FILE, LOG_ERROR_FILE);
+
+	if (_cgiConfig.cgiEnabled) {
+		_cgiPath = location.root + "/" + _cgiConfig.cgiPath;
+		if (location.cgiConfig.cgiExtension == EXTENSION_PHP) {
+			_cgiExecutable = "/usr/bin/php-cgi";
+		} else if (location.cgiConfig.cgiExtension == EXTENSION_PY) {
+			_cgiExecutable = "/usr/bin/python3";
+		}
+
+		logger.logDebug(LOG_INFO, "CGI Path: " + _cgiPath, true);
+		logger.logDebug(LOG_INFO, "CGI Executable: " + _cgiExecutable, true);
+
+		// _cgiExecutable = location.cgiConfig.cgiPath;
+		// _env["SERVER_PROTOCOL"] = request.getHttpVersion();
+		// _env["REQUEST_METHOD"] = request.getMethod();
+		// _env["REQUEST_URI"] = request.getUri();
+		// _env["SCRIPT_NAME"] = location.cgiConfig.cgiPath;
+		// _env["SCRIPT_FILENAME"] = location.root + location.cgiConfig.cgiPath;
+		// _env["PATH_INFO"] = request.getUri();
+	}
 }
 
-// Copy assignment overload
-CGI &CGI::operator=(const CGI &rhs) {
-  (void)rhs;
-  return *this;
-}
-
-// Default destructor
-CGI::~CGI() { return; }
+/* Destructor Method */
+CGI::~CGI( void ) {};
