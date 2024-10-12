6c11f3dd018b43744ad49d348b3a5a619796255d
Arthur Sobreira <tutusobreira@gmail.com>
2024-10-12 07:45:29 -0300

    Update CGI.cpp

--- src/CGI.cpp
+++ src/CGI.cpp
@@ -1,22 +1,57 @@
 
 #include "CGI.hpp"
 
+static bool	methodIsOnLocation( LocationConfigs &location, 
+	const std::string &method ) {
+	std::vector<httpMethod> methods = location.methods;
+
+	if (methods.empty()) { return true; }
+	for (std::vector<httpMethod>::iterator it = methods.begin(); 
+		it != methods.end(); ++it) {
+		if (*it == GET && method == "GET") { return true; }
+		if (*it == POST && method == "POST") { return true; }
+		if (*it == DELETE && method == "DELETE") { return true; }
+	}
+	return (false);
+}
+
+static std::string	intToString(int value) {
+	std::stringstream ss;
+	ss << value;
+	return ss.str();
+}
+
 /* Constructor Method */
-CGI::CGI( const Request &request, const LocationConfigs &location ) 
-	: _returnCode(200), _returnbody(DEFAULT_EMPTY), 
-	_request(request), _cgiConfig(location.cgiConfig) {
+CGI::CGI( const Request &request, const ServerConfigs &server,
+	const LocationConfigs &location ) 
+	: _returnCode(200), _returnbody(DEFAULT_EMPTY), _request(request), 
+	_serverConfig(server), _locationConfig(location) {
 	Logger logger(LOG_FILE, LOG_ACCESS_FILE, LOG_ERROR_FILE);
 
-	if (_cgiConfig.cgiEnabled) {
-		_cgiPath = location.root + "/" + _cgiConfig.cgiPath;
-		if (location.cgiConfig.cgiExtension == EXTENSION_PHP) {
+	if (!methodIsOnLocation(_locationConfig, _request.getMethod())) {
+		Response response;
+		std::string codeStr;
+
+		_returnCode = 405;
+		codeStr = intToString(_returnCode);
+		response.handleError(_returnCode, _serverConfig.errorPages[codeStr],
+			ERROR_METHOD_NOT_ALLOWED, logger);
+		_returnbody = response.generateResponse();
+		return;
+	}
+
+	if (_locationConfig.cgiEnabled) {
+		_cgiPath = location.root + "/" + location.cgiPath;
+		logger.logDebug(LOG_INFO, "213CGI Path: " + location.cgiPath, true);
+		if (location.cgiExtension == EXTENSION_PHP) {
 			_cgiExecutable = PHP_EXECUTABLE;
-		} else if (location.cgiConfig.cgiExtension == EXTENSION_PY) {
+		} else if (location.cgiExtension == EXTENSION_PY) {
 			_cgiExecutable = PYTHON_EXECUTABLE;
 		}
 
 		_setEnvironmentVars();
 
+		logger.logDebug(LOG_INFO, "Complete Request: \n" + _request.getRawRequest(), true);
 		logger.logDebug(LOG_INFO, "CGI Path: " + _cgiPath, true);
 		logger.logDebug(LOG_INFO, "CGI Executable: " + _cgiExecutable, true);
 		logger.logDebug(LOG_INFO, "CGI SERVER_PROTOCOL: " + _env["SERVER_PROTOCOL"], true);
@@ -26,6 +61,9 @@ CGI::CGI( const Request &request, const LocationConfigs &location )
 		logger.logDebug(LOG_INFO, "CGI SCRIPT_FILENAME: " + _env["SCRIPT_FILENAME"], true);
 		logger.logDebug(LOG_INFO, "CGI PATH_INFO: " + _env["PATH_INFO"], true);
 		logger.logDebug(LOG_INFO, "CGI QUERY_STRING: " + _env["QUERY_STRING"], true);
+		logger.logDebug(LOG_INFO, "CGI RAW_REQUEST: " + _env["RAW_REQUEST"], true);
+		logger.logDebug(LOG_INFO, "CGI CONTENT_LENGTH: " + _env["CONTENT_LENGTH"], true);
+		logger.logDebug(LOG_INFO, "CGI CONTENT_TYPE: " + _env["CONTENT_TYPE"], true);
 	}
 	(void)_returnCode;
 }
@@ -35,13 +73,34 @@ CGI::~CGI( void ) {};
 
 /* Private Methods */
 void	CGI::_setEnvironmentVars( void ) {
+	Logger logger(LOG_FILE, LOG_ACCESS_FILE, LOG_ERROR_FILE);
+
 	_env["SERVER_PROTOCOL"] = _request.getHttpVersion();
 	_env["REQUEST_METHOD"] = _request.getMethod();
 	_env["REQUEST_URI"] = _request.getUri();
-	_env["SCRIPT_NAME"] = _cgiConfig.cgiPath;
+	_env["SCRIPT_NAME"] = _locationConfig.cgiPath;
 	_env["SCRIPT_FILENAME"] = _cgiPath;
 	_env["PATH_INFO"] = _getPathInfo(_request.getUri());
 	_env["QUERY_STRING"] = _getQueryString(_request.getUri());
+	if (_request.getMethod() == "POST") {
+		std::string contentLength = _request.getHeader("Content-Length");
+		std::string contentType = _request.getHeader("Content-Type");
+
+		if (!contentLength.empty()) {
+			_env["CONTENT_LENGTH"] = contentLength;
+		} else { _env["CONTENT_LENGTH"] = DEFAULT_EMPTY; }
+
+		if (!contentType.empty()) {
+			_env["CONTENT_TYPE"] = contentType;
+		} else { _env["CONTENT_TYPE"] = "text/plain"; }
+	}
+}
+
+std::string CGI::_getContentLength( void ) const {
+	std::string contentLength = _request.getHeader("Content-Length");
+
+	if (!contentLength.empty()) { return contentLength; }
+	else { return DEFAULT_EMPTY; }
 }
 
 std::string	CGI::_getQueryString( const std::string &uri ) const {
@@ -52,7 +111,7 @@ std::string	CGI::_getQueryString( const std::string &uri ) const {
 }
 
 std::string	CGI::_getPathInfo( const std::string &uri ) const {
-	std::string scriptName = _cgiConfig.cgiPath;
+	std::string scriptName = _locationConfig.cgiPath;
 	std::size_t scriptPos = uri.find(scriptName);
 
 	if (scriptPos != std::string::npos) {
@@ -65,4 +124,13 @@ std::string	CGI::_getPathInfo( const std::string &uri ) const {
 		return (pathInfo.empty() ? "/" : pathInfo);
 	}
 	return (DEFAULT_LOCATION_PATH);
+}
+
+/* Public Methods */
+int	CGI::getReturnCode( void ) const {
+	return (_returnCode);
+}
+
+std::string	CGI::getReturnBody( void ) const {
+	return (_returnbody);
 }
