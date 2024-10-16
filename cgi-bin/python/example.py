2664bd0484e004f4fc28918cca2b7a78cc3ee34f
Arthur <tutusobreira@gmail.com>
2024-10-15 21:04:37 -0300

    Update cgi-bin

---
+++ cgi-bin/python/example.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import cgi
+
+form = cgi.FieldStorage()
+num1 = float(form.getvalue("num1", 0))
+num2 = float(form.getvalue("num2", 0))
+operation = form.getvalue("operation", "add")
+
+if operation == "add":
+    result = num1 + num2
+elif operation == "sub":
+    result = num1 - num2
+elif operation == "mul":
+    result = num1 * num2
+elif operation == "div" and num2 != 0:
+    result = num1 / num2
+else:
+    result = "Invalid Operation"
+
+print(f"<html><body><h1>Resultado: {result}</h1></body></html>")
+
+# "GET /cgi-bin/calculator.py?num1=2&num2=3&operation=add HTTP/1.1"
