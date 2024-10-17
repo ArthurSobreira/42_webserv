d8f1c457bd2b2e93b47e3db39a8641b903df0243
Arthur <tutusobreira@gmail.com>
2024-10-17 17:22:20 -0300

    Update Utils.cpp

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -153,3 +153,32 @@ void signals(int sig)
 	if (sig == SIGINT || sig == SIGQUIT || sig == SIGTERM)
 		throw std::runtime_error("bye bye");
 }
+
+namespace CGIUtils {
+	bool	methodIsOnLocation( LocationConfigs &location, 
+		const std::string &method ) {
+		std::vector<httpMethod> methods = location.methods;
+
+		if (methods.empty()) { return true; }
+		for (std::vector<httpMethod>::iterator it = methods.begin(); 
+			it != methods.end(); ++it) {
+			if (*it == GET && method == "GET") { return true; }
+			if (*it == POST && method == "POST") { return true; }
+			if (*it == DELETE && method == "DELETE") { return true; }
+		}
+		return (false);
+	}
+
+	std::string	intToString(int value) {
+		std::stringstream ss;
+		ss << value;
+		return ss.str();
+	}
+
+	void	deleteEnvp(char **envp) {
+		for (int i = 0; envp[i] != NULL; ++i) {
+			delete[] envp[i];
+		}
+		delete[] envp;
+	}
+}
