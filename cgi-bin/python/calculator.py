ef7b69c876d461326937c0621da473cc6533d4b7
Arthur <tutusobreira@gmail.com>
2024-10-17 23:30:20 -0300

    Update calculator.py

--- cgi-bin/python/calculator.py
+++ cgi-bin/python/calculator.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python3
+
 import cgi
 
-form = cgi.FieldStorage()
-num1 = float(form.getvalue("num1", 0))
-num2 = float(form.getvalue("num2", 0))
-operation = form.getvalue("operation", "add")
+def main() -> None:
+	form = cgi.FieldStorage()
+	num1 = float(form.getvalue("num1", 0))
+	num2 = float(form.getvalue("num2", 0))
+	operation = form.getvalue("operation", "add")
 
-if operation == "add":
-    result = num1 + num2
-elif operation == "sub":
-    result = num1 - num2
-elif operation == "mul":
-    result = num1 * num2
-elif operation == "div" and num2 != 0:
-    result = num1 / num2
-else:
-    result = "Invalid Operation"
+	if operation == "add":
+		result = num1 + num2
+	elif operation == "sub":
+		result = num1 - num2
+	elif operation == "mul":
+		result = num1 * num2
+	elif operation == "div" and num2 != 0:
+		result = num1 / num2
+	else:
+		result = "Invalid Operation"
 
-print(f"<html><body><h1>Resultado: {result}</h1></body></html>")
+	print(f"<html><body><h1>Resultado: {result}</h1></body></html>")
+
+if __name__ == "__main__":
+	main()
 
 # curl -X GET "http://localhost:4002/calculator?num1=10&num2=5&operation=div"
