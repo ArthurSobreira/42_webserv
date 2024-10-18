40369c8952d6fe1a0b9af3fbfc4c5b9a854d9794
Arthur <tutusobreira@gmail.com>
2024-10-17 23:31:21 -0300

    Update form.py

--- cgi-bin/python/form.py
+++ cgi-bin/python/form.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
+
 import cgi
 
-form = cgi.FieldStorage()
+def	main() -> None:
+	form = cgi.FieldStorage()
 
-print("<html><body>")
-if "name" in form and "age" in form:
-	name = form.getvalue("name")
-	age = form.getvalue("age")
-	print(f"<h1>Hello <u>{name}</u>, you are <u>{age}</u> years old!</h1>")
-else:
-	print("<h1>Error: Missing name or age</h1>")
+	print("<html><body>")
+	if "name" in form and "age" in form:
+		name = form.getvalue("name")
+		age = form.getvalue("age")
+		print(f"<h1>Hello <u>{name}</u>, you are <u>{age}</u> years old!</h1>")
+	else:
+		print("<h1>Error: Missing name or age</h1>")
 
-print("</body></html>")
+	print("</body></html>")
+
+if __name__ == "__main__":
+	main()
 
 # curl -X POST -d "name=John&age=30" http://localhost:4002/form
