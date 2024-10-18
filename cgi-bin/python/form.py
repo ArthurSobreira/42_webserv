b4a2a6d3f4b988bd042aaa5ab8d5a30661d32036
Arthur <tutusobreira@gmail.com>
2024-10-17 23:11:26 -0300

    Update form.py

--- cgi-bin/python/form.py
+++ cgi-bin/python/form.py
@@ -7,9 +7,10 @@ print("<html><body>")
 if "name" in form and "age" in form:
 	name = form.getvalue("name")
 	age = form.getvalue("age")
-	print(f"<h1>Hello <u>{name}</u>, \
-		you are <u>{age}</u> years old!</h1>")
+	print(f"<h1>Hello <u>{name}</u>, you are <u>{age}</u> years old!</h1>")
 else:
 	print("<h1>Error: Missing name or age</h1>")
 
 print("</body></html>")
+
+# curl -X POST -d "name=John&age=30" http://localhost:4002/form
