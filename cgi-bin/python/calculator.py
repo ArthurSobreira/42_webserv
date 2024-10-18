384f29b91e114d0f2ec03249123767fc4be45e6e
Arthur Sobreira <tutusobreira@gmail.com>
2024-10-18 14:59:24 -0300

    Update calculator.py

--- cgi-bin/python/calculator.py
+++ cgi-bin/python/calculator.py
@@ -4,22 +4,26 @@ import cgi
 
 def main() -> None:
 	form = cgi.FieldStorage()
-	num1 = float(form.getvalue("num1", 0))
-	num2 = float(form.getvalue("num2", 0))
-	operation = form.getvalue("operation", "add")
+	try:
+		num1 = float(form.getvalue("num1", 0))
+		num2 = float(form.getvalue("num2", 0))
+		operation = form.getvalue("operation", "add")
 
-	if operation == "add":
-		result = num1 + num2
-	elif operation == "sub":
-		result = num1 - num2
-	elif operation == "mul":
-		result = num1 * num2
-	elif operation == "div" and num2 != 0:
-		result = num1 / num2
-	else:
-		result = "Invalid Operation"
+		if operation == "add":
+			result = num1 + num2
+		elif operation == "sub":
+			result = num1 - num2
+		elif operation == "mul":
+			result = num1 * num2
+		elif operation == "div" and num2 != 0:
+			result = num1 / num2
+		else:
+			result = "Invalid Operation"
 
-	print(f"<html><body><h1>Resultado: {result}</h1></body></html>")
+		print(f"<html><body><h1>Result: {result}</h1></body></html>")
+
+	except ValueError:
+		print("<html><body><h1>Error: Invalid number</h1></body></html>")
 
 if __name__ == "__main__":
 	main()
