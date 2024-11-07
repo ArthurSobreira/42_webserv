667c2e48874c24351c9d5f8cb97be1513a3a3205
Arthur Sobreira <tutusobreira@gmail.com>
2024-11-07 16:15:53 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,16 @@
 
 volatile sig_atomic_t stop = 0;
 
-
 int main(int argc, char **argv)
 {
     if (argc != 2)
     {
         std::cerr << "Usage: " << argv[0] << " <config_file_path>" << std::endl;
-        return EXIT_FAILURE;
+        return (EXIT_FAILURE);
     }
 
+    Logger logger(LOG_FILE, LOG_ACCESS_FILE, LOG_ERROR_FILE);
     setupSignalHandlers();
-
     try
     {
         ServerManager serverManager(argv[1]);
@@ -29,9 +28,8 @@ int main(int argc, char **argv)
     }
     catch (const std::exception &e)
     {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return EXIT_FAILURE;
+        logger.logError(LOG_ERROR, e.what(), true);
+        return (EXIT_FAILURE);
     }
-
-    return EXIT_SUCCESS;
+    return (EXIT_SUCCESS);
 }
