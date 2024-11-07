751e3eee761ff210587a633da1672082fd7d76dc
Arthur Sobreira <tutusobreira@gmail.com>
2024-11-07 20:58:01 -0300

    Update EpollManager.hpp

--- include/EpollManager.hpp
+++ include/EpollManager.hpp
@@ -2,27 +2,31 @@
 #ifndef EPOLLMANAGER_HPP
 #define EPOLLMANAGER_HPP
 
-// Class declaration
+#include "Includes.hpp"
+#include "Defines.hpp"
 #include "Logger.hpp"
 #include "Fds.hpp"
 
-// Classe encapsulando a lógica de Epoll
-class EpollManager
-{
+class EpollManager {
+	private:
+		int	_epoll_fd;
+		Logger	&_logger;
+
 	public:
-		EpollManager(Logger &logger);
-		~EpollManager();
-		EpollManager &operator=(const EpollManager &rhs);
-		bool addToEpoll(int sockfd, uint32_t events);
-		bool removeFromEpoll(int sockfd);
-		bool modifyEpoll(int sockfd, uint32_t events);
-		int getEpollFD() const;
+		/* Constructor Method */
+		EpollManager( Logger &logger );
+
+		/* Destructor Method */
+		~EpollManager( void );
+
+		/* Public Methods */
+		bool	addToEpoll( int sockfd, uint32_t events );
+		bool	removeFromEpoll( int sockfd );
+		bool	modifyEpoll( int sockfd, uint32_t events );
+		int	getEpollFD( void ) const;
 
 	private:
-		int _epoll_fd;
-		Logger &_logger;
-		void initialize();
-
+		void	_initialize( void );
 };
 
 #endif // EPOLLMANAGER_HPP
