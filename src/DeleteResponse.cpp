14f3230f737beedaac782eeef978a8818624e280
Arthur Sobreira <tutusobreira@gmail.com>
2024-11-07 21:07:15 -0300

    Update DeleteResponse.cpp

--- src/DeleteResponse.cpp
+++ src/DeleteResponse.cpp
@@ -1,30 +1,27 @@
 #include "DeleteResponse.hpp"
 
-DeleteResponse::DeleteResponse(const std::string &filePath, ServerConfigs &server) 
+/* Constructor Method */
+DeleteResponse::DeleteResponse( const std::string &filePath, ServerConfigs &server ) 
 	: Response(), _filePath(filePath), _server(server) {}
 
-void DeleteResponse::prepareResponse()
-{
-	// Verifica se o arquivo existe e se temos permissão para removê-lo
-	if (access(_filePath.c_str(), F_OK) != 0)
-	{
-		handleError("404", _server.errorPages.at("404"), "Not Found", _logger);
-		return;
-	}
+/* Destructor Method */
+DeleteResponse::~DeleteResponse( void ) {}
 
-	if (access(_filePath.c_str(), W_OK) != 0)
-	{
-		handleError("403", _server.errorPages.at("403"), "Forbidden: No write permission", _logger);
+/* Public Methods */
+void DeleteResponse::prepareResponse( void ) {
+	if (access(_filePath.c_str(), F_OK) != 0) {
+		handleError("404", _server.errorPages.at("404"), ERROR_NOT_FOUND, _logger);
 		return;
 	}
-	// Tenta remover o arquivo
-	if (remove(_filePath.c_str()) == 0)
-	{
+	if (access(_filePath.c_str(), W_OK) != 0) {
+		handleError("403", _server.errorPages.at("403"), ERROR_FORBIDDEN, _logger);
+		return;
+	}
+	if (remove(_filePath.c_str()) == 0) {
 		_statusCode = "204";
 		_reasonPhrase = "No Content";
 	}
-	else
-	{
-		handleError("500", _server.errorPages.at("500"), "Internal Server Error", _logger);
+	else {
+		handleError("500", _server.errorPages.at("500"), ERROR_INTERNAL_SERVER, _logger);
 	}
 }
