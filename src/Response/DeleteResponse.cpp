56d2afe991afb529e9603f8d566ca54f2a022543
phm-aguiar <phenriq2@student.42sp.org.br>
2024-11-12 20:18:56 -0300

    I changed it to concatenate the uri with the location root

--- src/Response/DeleteResponse.cpp
+++ src/Response/DeleteResponse.cpp
@@ -1,20 +1,20 @@
 #include "DeleteResponse.hpp"
 
 /* Constructor Method */
-DeleteResponse::DeleteResponse( const std::string &filePath, ServerConfigs &server ) 
-	: Response(), _filePath(filePath), _server(server) {}
-
+DeleteResponse::DeleteResponse( const std::string &filePath, LocationConfigs location ) 
+	: Response(), _filePath(filePath), _location(location) {}
 /* Destructor Method */
 DeleteResponse::~DeleteResponse( void ) {}
 
 /* Public Methods */
 void DeleteResponse::prepareResponse( void ) {
+	_filePath = _location.root + _filePath; // por isso fica mais facil receber a location direto
 	if (access(_filePath.c_str(), F_OK) != 0) {
-		handleError("404", _server.errorPages.at("404"), ERROR_NOT_FOUND, _logger);
+		handleError("404", _location.server->errorPages.at("404"), ERROR_NOT_FOUND, _logger);
 		return;
 	}
 	if (access(_filePath.c_str(), W_OK) != 0) {
-		handleError("403", _server.errorPages.at("403"), ERROR_FORBIDDEN, _logger);
+		handleError("403", _location.server->errorPages.at("403"), ERROR_FORBIDDEN, _logger);
 		return;
 	}
 	if (remove(_filePath.c_str()) == 0) {
@@ -22,6 +22,6 @@ void DeleteResponse::prepareResponse( void ) {
 		_reasonPhrase = "No Content";
 	}
 	else {
-		handleError("500", _server.errorPages.at("500"), ERROR_INTERNAL_SERVER, _logger);
+		handleError("500", _location.server->errorPages.at("500"), ERROR_INTERNAL_SERVER, _logger);
 	}
 }
