1803ce96451d8870f4e79c4d1e3172b818660c6a
Pedro Henrique Modesto De Aguiar <phenriq2@student.42sp.org.br>
2024-11-13 16:30:27 -0300

    I didn't like putting this function here but that's what you have for today

--- include/ServerManager.hpp
+++ include/ServerManager.hpp
@@ -64,6 +64,8 @@ class ServerManager {
 		void	_closeConnection( int clientSocket );
 		void	_handleError( int clientSocket, const std::string &errorPage, 
 			const std::string &status, const std::string &error_message );
+		std::string _handleRedirect( const std::string &location );
+
 };
 
 #endif // SERVERMANAGER_HPP
